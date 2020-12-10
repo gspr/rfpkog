8272ed48bbdb50f42a4226a05b763b36a806bb87
Gard Spreemann <gspr@nonempty.org>
2020-12-10 09:51:26 +0100

    Move OpenCL preprocessor definitions to CMake.
    This way they won't be forgotten before some OpenCL header import
    somewhere else than main.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -18,11 +18,6 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-#define CL_HPP_MINIMUM_OPENCL_VERSION 120
-#define CL_HPP_TARGET_OPENCL_VERSION 120
-#define CL_HPP_CL_1_2_DEFAULT_BUILD
-#define CL_HPP_ENABLE_EXCEPTIONS 0
-
 #include <cmath>
 #include <fstream>
 #include <iomanip>
